94cfddc9ae30213706690ef6c3e47e220bf73d44
chebon <chebon85@gmail.com>
2018-10-21 19:01:00 +0300

    update form data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,32 @@
-new Vue({
-  el: '#app',
-  data () {
-    return {
-      info: null
-    }
+
+var action2 = new Vue({
+  el: '#action-2',
+  data: {
+    name: 'Vue.js'
   },
-  mounted () {
-    axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = response))
+  // define methods under the `methods` object
+  methods: {
+    verify_submit: function (event) {
+      alert('Hello ' + this.name + '!')
+      // `event` is the native DOM event
+      var student_name = $("#student_name").val()
+      var admission_number = $("#admission_number").val()
+      var fd = new FormData();
+      fd.append( 'student_name', student_name );
+      fd.append( 'admission_number', admission_number );
+      $.ajax({
+        url: 'http://example.com/script.php',
+        data: fd,
+        processData: false,
+        contentType: false,
+        type: 'POST',
+        success: function(data){
+          alert(data);
+        }
+      });
+
+    }
   }
 })
+
+// you can invoke methods in JavaScript too
