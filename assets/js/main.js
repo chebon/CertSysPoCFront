d5e82975a5a89e79b44f059d53ed4fe3f0ee305d
derrick rono <derrickrono@gmail.com>
2018-10-23 13:52:57 +0300

    Modified the loops and creations of post string

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,22 @@
 
 var show_units_grade = function(data){
   $('#transcript_unit').empty()
-  for each (var item in data) {
+  for (j=0; j<data.length;j++) 
+  {
 
     var unit= '<tr class="bg-white">'+
-        '<td style="color: black;" >'+item.name+'</td>'+
-        '<td style="color: black;" >'+item.score+'</td>'+
+        '<td style="color: black;" >'+data[j].name+'</td>'+
+        '<td style="color: black;" >'+data[j].score+'</td>'+
         '</tr>'+
     '<tr class="table-divider"></tr>'
     $('#transcript_unit').append(unit)
   }
 
-}
+};
 
 var transripts_append = function(data){
-for each (var transcript in data) {
+for (i=0; i<data.length;i++) 
+{
   var transcript_dom = '<div class="col-lg-3 mb-5 mb-lg-0">'+
       '<div data-animate-hover="1" data-toggle="hidden">'+
           '<div class="card bg-gradient-primary shadow-primary border-primary animate-this">'+
@@ -22,9 +24,9 @@ for each (var transcript in data) {
                   '<img alt="Image placeholder" src="./assets/img/icons/dusk/svg/browser-window.svg" class="img-saturate" style="width: 100px;">'+
               '</div>'+
               '<div class="px-4 pb-5 text-center">'+
-                  '<a href="#sct_page_examples" onclick="show_units_grade('+transcript.data+')"  data-toggle="modal" data-target="#modal_5" class="btn btn-sm btn-white btn-circle">View Units Score</a>'+
-                  '<p style=" font-size: 19px; margin: 0px; "  class="text-white mt-4">'+transcript.year+'</p>'+
-                  '<p style=" margin-top: 12px; " class="text-white">'+transcript.grade+'</p>'+
+                  '<a href="#sct_page_examples" onclick="show_units_grade('+data[i].data+')"  data-toggle="modal" data-target="#modal_5" class="btn btn-sm btn-white btn-circle">View Units Score</a>'+
+                  '<p style=" font-size: 19px; margin: 0px; "  class="text-white mt-4">'+data[i].year+'</p>'+
+                  '<p style=" margin-top: 12px; " class="text-white">'+data[i].grade+'</p>'+
               '</div>'+
           '</div>'+
       '</div>'+
@@ -33,7 +35,7 @@ for each (var transcript in data) {
   $('#transcript_container').append(transcript_dom)
 
 }
-}
+};
 
 var append_results = function(data) {
   $('#course_taken').empty().append(data.data.course_taken)
@@ -47,7 +49,7 @@ var append_results = function(data) {
 var process_errors = function() {
   alert("sorry an error try again later")
   transripts_append('data')
-}
+};
 
 
 
@@ -63,15 +65,14 @@ var action2 = new Vue({
       // `event` is the native DOM event
       var student_name = $("#student_name").val()
       var admission_number = $("#admission_number").val()
-      var fd = new FormData();
-      fd.append( 'student_name', student_name );
-      fd.append( 'admission_number', admission_number );
+      studentProfile= JSON.stringify({"fullname":student_name,"admission_number":admission_number}),
       $.ajax({
         url: 'http://127.0.0.1:8000/results',
-        data: fd,
+        type: 'POST',
+        data: studentProfile,
+        dataType: 'json',
         processData: false,
         contentType: false,
-        type: 'POST',
         success: function(data){
           append_results(data)
         },
@@ -83,6 +84,6 @@ var action2 = new Vue({
     }
   }
 
-})
+});
 
 // you can invoke methods in JavaScript too
